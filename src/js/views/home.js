ba101173a452cfcb3b2c03090e3796fd395ad18a
Outdoor-Dev <saltairpix@gmail.com>
2021-01-19 00:36:51 +0000

    update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,19 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
+import { characterCard } from "../component/Cards";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const [characters, setCharacters] = useState([{ name: "luke" }, { name: "darth vader" }]);
+	const [planets, setPlanets] = useState([{ name: "Death Star" }, { name: "The Republic" }]);
+
+	return (
+		<div className="text-center mt-5">
+			<h1>Characters</h1>
+			<div>
+				{characters.map((value, index) => {
+					return <Card key={index} character={value} />;
+				})}
+			</div>
+		</div>
+	);
+};
