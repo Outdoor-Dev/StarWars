ed99ba796c60255d859dc694280f0011c483a2d7
Outdoor-Dev <saltairpix@gmail.com>
2021-01-20 09:08:41 +0000

    update

--- src/js/component/CharacterCard.js
+++ src/js/component/CharacterCard.js
@@ -4,11 +4,11 @@ import PropTypes from "prop-types";
 export const CharacterCard = props => {
 	return (
 		<>
-			<div className="charactercard text-white ml-5 " style={{ width: "18rem" }}>
+			<div className="charactercard background-card text-white ml-5 mb-5 " style={{ width: "19rem" }}>
 				<img
 					src="https://static.wikia.nocookie.net/worldsgreatestheroes/images/9/91/Obi-Wan_Headshot.jpg/revision/latest/scale-to-width-down/340?cb=20140512143941"
 					className="card-img-top"
-					style={{ width: 300, height: 400 }}
+					style={{ width: 305, height: 400 }}
 					alt="..."
 				/>
 				<div className="card-body ">
@@ -26,9 +26,10 @@ export const CharacterCard = props => {
 						{props.character.hair_color}
 					</p>
 
-					<a href="#" className="btn btn-primary">
-						Go somewhere
+					<a href="#" className="btn btn-outline-light text-white">
+						Learn More
 					</a>
+					<i className="far fa-heart fa-lg " />
 				</div>
 			</div>
 		</>
--- src/js/component/PlanetCard.js
+++ src/js/component/PlanetCard.js
@@ -4,19 +4,20 @@ import PropTypes from "prop-types";
 export const PlanetCard = props => {
 	return (
 		<>
-			<div className="planet-card text-white ml-5 " style={{ width: "18rem" }}>
+			<div className="planet-card background-card text-white ml-5 mb-5 " style={{ width: "19rem" }}>
 				<img
 					src="https://media.cntraveler.com/photos/5727696bb161ad8b31235293/16:9/w_2560%2Cc_limit/GettyImages-143687829-tunisia.jpg"
 					className="card-img-top"
 					alt="..."
-					style={{ width: 300, height: 200 }}
+					style={{ width: 305, height: 200 }}
 				/>
 				<div className="card-body">
 					<h5 className="card-title">{props.planet.name}</h5>
 					<p className="card-text" />
-					<a href="#" className="btn btn-primary">
-						Go somewhere
+					<a href="#" className="btn btn-outline-light text-white">
+						Learn More
 					</a>
+					<i className="far fa-heart" />
 				</div>
 			</div>
 		</>
